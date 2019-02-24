0ea9693febeff5643b9b0e0d167ceeb537aa1d59
Nguyễn Văn Thiên <nvthien.uit.k10@gmail.com>
2019-02-24 18:17:47 +0700

    init project

---
+++ MapApp/MapApp/MapApp.Android/CustomMapRenderer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MapApp;
+using MapApp.Droid;
+using MapApp.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.Android;
+
+[assembly: ExportRenderer(typeof(CustomMap), typeof(CustomMapRenderer))]
+namespace MapApp.Droid
+{
+    
+    public class CustomMapRenderer : MapRenderer, GoogleMap.IInfoWindowAdapter
+    {
+        List<CustomPin> customPins;
+        public CustomMapRenderer(Context context) : base(context)
+        {
+
+        }
+        protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Map> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                NativeMap.InfoWindowClick -= OnInfoWindowClick;
+            }
+
+            if (e.NewElement != null)
+            {
+                var formsMap = (CustomMap)e.NewElement;
+                customPins = formsMap.CustomPins;
+                Control.GetMapAsync(this);
+            }
+        }
+
+        public void OnInfoWindowClick(object sender, GoogleMap.InfoWindowClickEventArgs e)
+        {
+            var customPin = GetCustomPin(e.Marker);
+            if (customPin == null)
+            {
+                throw new Exception("Custom pin not found");
+            }
+
+            if (!string.IsNullOrWhiteSpace(customPin.Url))
+            {
+                var url = Android.Net.Uri.Parse(customPin.Url);
+                var intent = new Intent(Intent.ActionView, url);
+                intent.AddFlags(ActivityFlags.NewTask);
+                Android.App.Application.Context.StartActivity(intent);
+            }
+        }
+
+        protected override void OnMapReady(GoogleMap map)
+        {
+            base.OnMapReady(map);
+
+            NativeMap.InfoWindowClick += OnInfoWindowClick;
+            NativeMap.SetInfoWindowAdapter(this);
+            NativeMap.MapLongClick += MapLongClick;
+        }
+
+        private void MapLongClick(object sender, GoogleMap.MapLongClickEventArgs e)
+        {
+            var pin = new CustomPin
+            {
+                Type = PinType.Place,
+                Address = "Test address",
+                Label = "Test Lable",
+                Position = new Position(e.Point.Latitude, e.Point.Longitude),
+                Id = "Xamarin",
+                Url = "http://xamarin.com/about/"
+            };
+            
+            customPins.Add(pin);
+            
+           
+        }
+
+        protected override MarkerOptions CreateMarker(Pin pin)
+        {
+            var marker = new MarkerOptions();
+            marker.SetPosition(new LatLng(pin.Position.Latitude, pin.Position.Longitude));
+            marker.SetTitle(pin.Label);
+            marker.SetSnippet(pin.Address);
+            marker.SetIcon(BitmapDescriptorFactory.FromResource(Resource.Drawable.pin));
+            return marker;
+        }
+
+        Android.Views.View GoogleMap.IInfoWindowAdapter.GetInfoContents(Marker marker)
+        {
+            var inflater = Android.App.Application.Context.GetSystemService(Context.LayoutInflaterService) as Android.Views.LayoutInflater;
+            if (inflater != null)
+            {
+                Android.Views.View view;
+
+                var customPin = GetCustomPin(marker);
+                if (customPin == null)
+                {
+                    throw new Exception("Custom pin not found");
+                }
+
+                if (customPin.Id.ToString() == "Xamarin")
+                {
+                    view = inflater.Inflate(Resource.Layout.MapInfoWindow, null);
+                }
+                else
+                {
+                    view = inflater.Inflate(Resource.Layout.MapInfoWindow, null);
+                }
+
+                var infoTitle = view.FindViewById<TextView>(Resource.Id.InfoWindowTitle);
+                var infoSubtitle = view.FindViewById<TextView>(Resource.Id.InfoWindowSubtitle);
+
+                if (infoTitle != null)
+                {
+                    infoTitle.Text = marker.Title;
+                }
+                if (infoSubtitle != null)
+                {
+                    infoSubtitle.Text = marker.Snippet;
+                }
+
+                return view;
+            }
+            return null;
+        }
+
+        CustomPin GetCustomPin(Marker annotation)
+        {
+            var position = new Position(annotation.Position.Latitude, annotation.Position.Longitude);
+            foreach (var pin in ((CustomMap)Element).CustomPins)
+            {
+                if (pin.Position == position)
+                {
+                    return pin;
+                }
+            }
+            return null;
+        }
+
+        Android.Views.View GoogleMap.IInfoWindowAdapter.GetInfoWindow(Marker marker)
+        {
+            return null;
+        }
+    }
+}
---
+++ MapApp/MapApp/MapApp/MapPage.cs
@@ -0,0 +1,96 @@
+﻿using MapApp.Models;
+using Plugin.Geolocator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace MapApp
+{
+    public class MapPage : ContentPage
+    {
+        private double Latitude;
+        private double Longitude;
+        public MapPage()
+        {
+            GetLocation();
+            
+        }
+
+        public async void GetLocation()
+        {
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+
+                var position = await locator.GetPositionAsync();
+                Latitude = position.Latitude;
+                Longitude = position.Longitude;
+            }
+            catch(Exception ex)
+            {
+
+            }
+            
+            CreateMap();
+        }
+
+        public void CreateMap()
+        {
+            var currentMap = new CustomMap
+            {
+                HasScrollEnabled = true,
+                HasZoomEnabled = true,
+                MapType = MapType.Street,
+                WidthRequest = App.ScreenWidth,
+                HeightRequest = App.ScreenHeight
+            };
+
+            var pin = new CustomPin
+            {
+                Type = PinType.Place,
+                Address = "Linh Trung, Thu Duc, Ho Chi Minh",
+                Label = "University Infomation of Technology",
+                Position = new Position(10.879683, 106.803269),
+                Id = "Xamarin",
+                Url = "http://xamarin.com/about/"
+            };
+
+            var pin1 = new CustomPin
+            {
+                Type = PinType.Place,
+                Address = "Linh Trung, Thu Duc, Ho Chi Minh",
+                Label = "University Infomation of Technology",
+                Position = new Position(5.879683, 100.803269),
+                Id = "Xamarin",
+                Url = "http://xamarin.com/about/"
+            };
+            currentMap.CustomPins = new List<CustomPin> { pin,pin1 };
+
+            currentMap.Pins.Add(pin);          
+
+            //pin.Clicked += UnivercityInfoAndTech_clicked;
+
+
+            currentMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(Latitude, Longitude), 
+                Distance.FromMiles(1.0)));
+
+            
+            Content = currentMap;
+        }
+
+        private void CurrentMap_focuesed(object sender, FocusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public void UnivercityInfoAndTech_clicked(object sender, EventArgs e)
+        //{
+        //    var selectedPin = (Pin)sender;
+        //    DisplayAlert(selectedPin.Label, selectedPin.Address, "OK");
+        //}
+    }
+}
